129d51d4349c14d383dc9a9e7360e84d5a19273e
ShuzhiLiu <liushuzhi2014@gmail.com>
2020-02-23 12:13:25 +0900

    add reboot

--- install_step2.sh
+++ install_step2.sh
@@ -21,3 +21,5 @@ sudo apt-get install --no-install-recommends \
 sudo apt-get install -y --no-install-recommends libnvinfer6=6.0.1-1+cuda10.1 \
     libnvinfer-dev=6.0.1-1+cuda10.1 \
     libnvinfer-plugin6=6.0.1-1+cuda10.1
+
+sudo reboot
