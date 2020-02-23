0a78663ec8c5bea99ec8c6d1c3a48d7bcf91295b
ShuzhiLiu <liushuzhi2014@gmail.com>
2020-02-23 12:08:45 +0900

    initial commit

---
+++ install_step1.sh
@@ -0,0 +1,30 @@
+# Reset environment
+# Purge existign CUDA first
+sudo apt --purge remove "cublas*" "cuda*"
+sudo apt --purge remove "nvidia*"
+
+### If you have previous installation remove it first. 
+sudo apt-get purge nvidia*
+sudo apt remove nvidia-*
+sudo rm /etc/apt/sources.list.d/cuda*
+sudo apt-get autoremove && sudo apt-get autoclean
+sudo rm -rf /usr/local/cuda*
+
+### to verify your gpu is cuda enable check
+lspci | grep -i nvidia
+
+### gcc compiler is required for development using the cuda toolkit. to verify the version of gcc install enter
+gcc --version
+
+# system update
+sudo apt-get update
+sudo apt-get upgrade
+
+# install other import packages
+sudo apt-get install g++ freeglut3-dev build-essential libx11-dev libxmu-dev libxi-dev libglu1-mesa libglu1-mesa-dev
+
+# === install driver ===
+sudo lshw -c display
+sudo ubuntu-drivers devices
+sudo ubuntu-drivers autoinstall
+sudo reboot
