e0e9350d2b2d5ca92b309cc8d25239d4914443ab
ShuzhiLiu <liushuzhi2014@gmail.com>
2021-01-31 18:53:05 +0900

    update

--- TestPytorch.py
+++ TestPytorch.py
@@ -3,9 +3,9 @@ import torch
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print('Using device:', device)
 
-#Additional Info when using cuda
+# Additional Info when using cuda
 if device.type == 'cuda':
     print(torch.cuda.get_device_name(0))
     print('Memory Usage:')
-    print('Allocated:', round(torch.cuda.memory_allocated(0)/1024**3,1), 'GB')
-    print('Cached:   ', round(torch.cuda.memory_cached(0)/1024**3,1), 'GB')
+    print('Allocated:', round(torch.cuda.memory_allocated(0) / 1024 ** 3, 1), 'GB')
+    print('Cached:   ', round(torch.cuda.memory_cached(0) / 1024 ** 3, 1), 'GB')
--- TestTF.py
+++ TestTF.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 
 print(
     tf.test.is_gpu_available(
-    cuda_only=False,
-    min_cuda_compute_capability=None
-)
+        cuda_only=False,
+        min_cuda_compute_capability=None
+    )
 )
