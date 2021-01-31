c558c921c9b8ee8d89c23ca36e8930fd85820b36
ShuzhiLiu <liushuzhi2014@gmail.com>
2021-01-31 18:51:45 +0900

    update

--- install_step1.sh
+++ install_step1.sh
@@ -3,7 +3,7 @@
 sudo apt --purge remove "cublas*" "cuda*"
 sudo apt --purge remove "nvidia*"
 
-### If you have previous installation remove it first. 
+### If you have previous installation remove it first.
 sudo apt-get purge nvidia*
 sudo apt remove nvidia-*
 sudo rm /etc/apt/sources.list.d/cuda*
@@ -23,8 +23,19 @@ sudo apt-get upgrade
 # install other import packages
 sudo apt-get install g++ freeglut3-dev build-essential libx11-dev libxmu-dev libxi-dev libglu1-mesa libglu1-mesa-dev
 
-# === install driver ===
-sudo lshw -c display
-sudo ubuntu-drivers devices
-sudo ubuntu-drivers autoinstall
+# === From TensorFlow HP ===
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
+sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
+sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
+sudo apt-get update
+
+wget http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1804/x86_64/nvidia-machine-learning-repo-ubuntu1804_1.0.0-1_amd64.deb
+
+sudo apt install ./nvidia-machine-learning-repo-ubuntu1804_1.0.0-1_amd64.deb
+sudo apt-get update
+
+# Install NVIDIA driver
+sudo apt-get install --no-install-recommends nvidia-driver-450
+
 sudo reboot
